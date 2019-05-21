0098acad84cfafe47dbb0c7153109cf18d51dd9b
Foel <petri.vihavainen@gmail.com>
2019-05-21 12:44:50 +0300

    Intro valmis

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -4,20 +4,32 @@ import ReactDOM from 'react-dom';
 const Hello = (props) => {
     return (
         <div>
-            if (props.nimi = null)
-                <p>Hello {props.name}</p>
-            else
-                <p>Hei {props.nimi}</p>
+            <p>
+                Hello {props.name}, you are {props.age} years old.
+            </p>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>>Created footer for the page</h1>
         </div>
     )
 }
 
 const App = () => {
+    console.log('Hello fron komponentti')
+    const name ='Pekka'
+    const age = 12
+
     return (
         <div>
             <h1>Greetings</h1>
-            <Hello nimi ="Antti"/>
-            <Hello name ="Pekka"/>
+            <Hello name ="Pekka" age = {20 + 13} />
+            <Hello name ={name} age = {age + 2} />
+            <Footer />
         </div>
     )
 }
