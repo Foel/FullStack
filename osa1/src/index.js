7d2f530be6d291c8fb2ee2bb30991db2b27fa8dd
Foel <petri.vihavainen@gmail.com>
2019-09-28 20:35:14 +0300

    Tested different formats

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 const Hello = (props) => {
+    console.log('Hello called')
     return (
         <div>
             <p>
@@ -12,24 +14,27 @@ const Hello = (props) => {
 }
 
 const Footer = () => {
+    console.log('Footer called')
     return (
         <div>
-            <h1>>Created footer for the page</h1>
+            <h1>Created footer for the page</h1>
+            <h4>Smaller text</h4>
         </div>
     )
 }
 
 const App = () => {
-    console.log('Hello fron komponentti')
-    const name ='Pekka'
+    console.log('Hello App komponentti')
+    const name ='Risto'
     const age = 12
 
     return (
         <div>
             <h1>Greetings</h1>
+            <Hello />
             <Hello name ="Pekka" age = {20 + 13} />
             <Hello name ={name} age = {age + 2} />
-            <Hello />
+            <Hello age = {4} />
             <Footer />
         </div>
     )
